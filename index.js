81082176a59b38d4950a0446a30e2953e61e7a40
Matt Fysh <mattfysh@gmail.com>
2015-06-15 00:33:49 -0700

    functional

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 var postcss = require('postcss');
 
+function getSelLength(node) {
+	if (node.type === 'rule') {
+		return node.selectors.length;
+	}
+	if (node.type === 'atrule') {
+		return 1 + node.nodes.reduce(function(memo, n) {
+			return memo + getSelLength(n);
+		}, 0);
+	}
+	return 0; // comment
+}
+
 module.exports = postcss.plugin('postcss-chunk', function (opts) {
-    opts = opts || {};
+	opts = opts || {};
+	var size = opts.size || 4000;
 
-    // Work with options here
+	return function(css, result) {
+		var plugins = result.processor.plugins;
+		var chunks = [];
+		var count, chunk;
 
-    return function (css) {
+		function nextChunk() {
+			count = 0;
+			chunk = css.clone({nodes: []});
+			chunks.push(chunk);
+		}
 
-        // Transform CSS AST here
+		if (plugins[plugins.length - 1].postcssPlugin !== 'postcss-chunk') {
+			throw new Error('postcss-chunk must be the last processor plugin');
+		}
 
-    };
+		css.nodes.forEach(function(n) {
+			var selCount = getSelLength(n);
+			if (!chunk || count + selCount > size) {
+				nextChunk();
+			}
+			chunk.nodes.push(n);
+			count += selCount;
+		});
+
+		result.chunks = chunks.map(function(c) {
+			return c.toResult();
+		});
+	};
 });
+	
--- test/test.js
+++ test/test.js
@@ -1,24 +1,63 @@
+var fs = require('fs');
 var postcss = require('postcss');
 var expect  = require('chai').expect;
 
 var plugin = require('../');
 
-var test = function (input, output, opts, done) {
-    postcss([ plugin(opts) ]).process(input).then(function (result) {
-        expect(result.css).to.eql(output);
-        expect(result.warnings()).to.be.empty;
-        done();
-    }).catch(function (error) {
+var input = fs.readFileSync(__dirname + '/input.css', 'utf-8');
+
+var test = function (input, opts, done) {
+    postcss([ plugin(opts) ]).process(input).then(function(result) {
+        done(null, result);
+    }).catch(function(error) {
         done(error);
     });
 };
 
 describe('postcss-chunk', function () {
 
-    /* Write tests here
+    it('breaks input into chunks of max size', function (done) {
+        test(input, {size: 5}, function(err, result) {
+            if (err) return done(err);
+            var chunks = result.chunks;
+            expect(chunks.length).to.equal(3);
+            expect(chunks[0].root.nodes.length).to.equal(5);
+            expect(chunks[2].root.nodes.length).to.equal(2);
+            done();
+        });
+    });
 
-    it('does something', function (done) {
-        test('a{ }', 'a{ }', { }, done);
-    });*/
+    it('counts multiple selectors per rule', function(done) {
+        var newInput = input.replace('two', 'two-a, two-b');
+        test(newInput, {size: 5}, function(err, result) {
+            if (err) return done(err);
+            var chunks = result.chunks;
+            expect(chunks.length).to.equal(3);
+            expect(chunks[0].root.nodes.length).to.equal(4);
+            expect(chunks[2].root.nodes.length).to.equal(3);
+            done();
+        });
+    });
 
+    it('counts at-rules as one rule', function(done) {
+        var newInput = input + '@media print { a {}, b {}, c {} }';
+        test(newInput, {size: 5}, function(err, result) {
+            if (err) return done(err);
+            var chunks = result.chunks;
+            expect(chunks.length).to.equal(4);
+            expect(chunks[3].root.nodes.length).to.equal(1);
+            done();
+        });
+    });
+
+    it('supports nested at-rules', function(done) {
+        var newInput = input + '@media print { @media print { a {}, b {}, c {} } }';
+        test(newInput, {size: 5}, function(err, result) {
+            if (err) return done(err);
+            var chunks = result.chunks;
+            expect(chunks.length).to.equal(4);
+            expect(chunks[3].root.nodes.length).to.equal(1);
+            done();
+        });
+    });
 });
